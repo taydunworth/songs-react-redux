2d4474d42cf359eb0bb5c861d2a65b1a23d95576
Taylor <taylorashtoncox@gmail.com>
2019-03-19 12:20:45 -0400

    Adds reducers including static songs

---
+++ src/reducers/index.js
@@ -0,0 +1,32 @@
+const songsReducer = () => {
+    return [
+        {
+            title: 'A Whole New World',
+            movie: 'Aladdin',
+            year: 1992
+        },
+        {
+            title: 'Part of Your World',
+            movie: 'The Little Mermaid',
+            year: 1989
+        },
+        {
+            title: 'Hakuna Matata',
+            movie: 'The Lion King',
+            year: 1994
+        },
+        {
+            title: 'Colors of the Wind',
+            movie: 'Pocahontas',
+            year: 1995
+        }
+    ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+
+    return selectedSong;
+};
